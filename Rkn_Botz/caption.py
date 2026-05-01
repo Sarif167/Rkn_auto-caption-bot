259f90a488a3afb00269f0c678abb71e9fd95071
Botmaster55 <wajsarif461@gmail.com>
2026-05-01 13:22:39 +0530

    Update caption.py

--- Rkn_Botz/caption.py
+++ Rkn_Botz/caption.py
@@ -112,9 +112,9 @@ async def start_cmd(client, message):
             f"Note: Commands only work in channels where I'm admin.</b>"
         ),
         reply_markup=types.InlineKeyboardMarkup([
-            [types.InlineKeyboardButton("📢 Main Channel", url="https://t.me/Rkn_Bots_Updates")],
-            [types.InlineKeyboardButton("❓ Help Group", url="https://t.me/Rkn_Bots_Support")],
-            [types.InlineKeyboardButton("🔥 Source Code", url="https://github.com/RknDeveloper/Rkn-AutoCaptionBot")]
+            [types.InlineKeyboardButton("📢 Main Channel", url="https://t.me/Prime_Movie_Request_bot")],
+            [types.InlineKeyboardButton("❓ Help Support", url="https://t.me/Prime_Movie_Request_bot")],
+            [types.InlineKeyboardButton("🔥 Source Code", url="https://t.me/Prime_Movie_Request_bot")]
         ])
     )
 
